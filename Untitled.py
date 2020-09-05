66d4e9fcc6847295d2bcafbfeb978db0640485cf
nayanimuppidi <70577579+nayanimuppidi@users.noreply.github.com>
2020-09-05 08:12:22 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first name = 'Faiyaz'
+
+
+# In[5]:
+
+
+first_name = 'Faiyaz'
+
+
+# In[6]:
+
+
+print(first_name)
+
+
+# In[7]:
+
+
+firstname = 'Faiyaz'
+
+
+# In[8]:
+
+
+print(firstname)
+
+
+# In[9]:
+
+
+1name='nayani'
+
+
+# In[10]:
+
+
+name1='nayani'
+
+
+# In[11]:
+
+
+print(name1)
+
+
+# In[12]:
+
+
+@name='nayani'
+
+
+# In[13]:
+
+
+name='nayani muppidi'
+
+
+# In[14]:
+
+
+print(name)
+
+
+# In[15]:
+
+
+print(name.title())
+
+
+# In[16]:
+
+
+print(name.upper())
+
+
+# In[17]:
+
+
+print(name.lower())
+
+
+# In[ ]:
+
+
+
+
+
+# In[18]:
+
+
+firstname='nayani'
+
+
+# In[19]:
+
+
+lastname='muppidi'
+
+
+# In[20]:
+
+
+full_name=f"(firstname)(lastname)"
+
+
+# In[21]:
+
+
+print(full_name)
+
+
+# In[25]:
+
+
+fullname=f"{firstname} {lastname}"
+
+
+# In[26]:
+
+
+print(fullname)
+
+
+# In[27]:
+
+
+print(fullname.title())
+
+
+# In[28]:
+
+
+print(f"Hello, {fullname.title()}")
+
+
+# In[29]:
+
+
+print("fav_langs:pythonc++javarubypearl")
+
+
+# In[30]:
+
+
+print("fav_langs:\npython\nc++\njava\nruby\npearl")
+
+
+# In[31]:
+
+
+print("fav_langs:\n\tpython\n\tc++\n\tjava\n\truby\n\tpearl")
+
+
+# In[ ]:
+
+
+
+
