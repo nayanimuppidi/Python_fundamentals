fea56bad2789e4192d72a6654eaaca8ee9447260
nayanimuppidi <70577579+nayanimuppidi@users.noreply.github.com>
2020-09-05 08:22:19 +0530

    Add files via upload

---
+++ Day_4 B27.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+language=' Python'
+
+
+# In[6]:
+
+
+print(language)
+
+
+# In[7]:
+
+
+language.lstrip()
+
+
+# In[8]:
+
+
+language.rstrip()
+
+
+# In[9]:
+
+
+lang="python "
+
+
+# In[10]:
+
+
+lang.rstrip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+student=['abc','def','ghi','jkl','mno','pqr',]
+
+
+# In[12]:
+
+
+print(student)
+
+
+# In[13]:
+
+
+type(student)
+
+
+# In[14]:
+
+
+print(student[1])
+
+
+# In[15]:
+
+
+print(student[1].title())
+
+
+# In[ ]:
+
+
+
+
